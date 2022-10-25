4e5a860ca1aa5f946d6a9d13639f5be1fc3c5b3b
n_narcise <joaquin.narcisse@gmail.com>
2022-10-25 18:45:19 -0500

    half-finished

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,7 +2,12 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sum = 0;
 
+  for (let i = 0; i < num.length; i++) {
+     sum += +num[i] * 2 ** (num.length - 1 - i);
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
