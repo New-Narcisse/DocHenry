3c6b7fb3d39e447186e8c423c1bf2165b58852cb
n_narcise <joaquin.narcisse@gmail.com>
2022-11-02 20:16:14 -0500

    Add hmwk de JS II

--- FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
+++ FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
@@ -47,6 +47,14 @@ function cacheFunction(cb) {
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
+    return function (arg){
+      if(cb.hasOwnProperty(arg.toString())){
+        return cb[arg.toString()]
+      }else {
+        cb[arg.toString()] = cb(arg);
+        return cb[arg.toString()];
+      }
+    }
 }
 
 // Bind
@@ -73,8 +81,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
@@ -82,13 +90,16 @@ let getNombreAlumno;
   Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
 */
 
+
+
+
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
+let textoAsteriscos = crearCadena.bind(undefined, '*', '*');
+let textoGuiones = crearCadena.bind(undefined, '-', '-');
+let textoUnderscore = crearCadena.bind(undefined, '_', '_');
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
