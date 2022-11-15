34e93808eaedaf0235f8ff3f1954e962785dd932
n_narcise <joaquin.narcisse@gmail.com>
2022-11-14 21:22:36 -0500

    test

--- FT-M1-master/EXTRA-Testing/homework/homework.js
+++ FT-M1-master/EXTRA-Testing/homework/homework.js
@@ -1,25 +1,17 @@
 
+const layout = [
+    [{type: 'VIP', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}],
+    [{type: 'NORMAL', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}, {type: 'NORMAL', booked: false}],
+    [{type: 'NORMAL', booked: false}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: false}],
+    [{type: 'ECONOMIC', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'ECONOMIC', booked: false}],
+    [{type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: true}, {type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: false}]
+];
 
-describe('checkSeatStatus',() => {
+
     let checkSeatStatus = function (row, number) {
         if(typeof row !== 'string') throw new TypeError('First parameter it`s not a string');
         if(typeof number !== 'number') throw new TypeError('Second parameter it`s not a number')
     
-        const layout = [
-            [{type: 'VIP', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}],
-            [{type: 'NORMAL', booked: false}, {type: 'VIP', booked: true}, {type: 'VIP', booked: false}, {type: 'NORMAL', booked: false}],
-            [{type: 'NORMAL', booked: false}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: false}],
-            [{type: 'ECONOMIC', booked: true}, {type: 'NORMAL', booked: true}, {type: 'NORMAL', booked: true}, {type: 'ECONOMIC', booked: false}],
-            [{type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: true}, {type: 'ECONOMIC', booked: false}, {type: 'ECONOMIC', booked: false}]
-        ];
-    
-        
-    
-    describe('getRowNumber', () => {
-        let getRowNumber = function (letter){
-            return letter.charCodeAt(0) - 65;
-            }
-    })
     
         let numberRow = getRowNumber(row);
         let layoutRows = layout[numberRow];
@@ -27,7 +19,23 @@ describe('checkSeatStatus',() => {
         return seat.booked;
     
     }
-   
-})
-module.exports = { checkSeatStatus }
+
+
+
+
+    let getRowNumber = function (letter){
+        return letter.charCodeAt(0) - 65;
+        }
+
+function book (){
+    if(checkSeatStatus(row,number)) return `Seat in ${row}${number} is already booked`;
+    const numberRow = getRowNumber(row);
+    const layoutRows = layout[numberRow];
+    const seat = layoutRows[number];
+    seat.booked = true;
+    return `Seat in ${row}${number} successfully booked`
+}
+
+
+module.exports = { checkSeatStatus, getRowNumber, book }
 
--- FT-M1-master/EXTRA-Testing/homework/tests/index.test.js
+++ FT-M1-master/EXTRA-Testing/homework/tests/index.test.js
@@ -1,35 +1,52 @@
-const {checkSeatStatus, getRowNumber} = require('../homework.js')
+const {checkSeatStatus, getRowNumber, book} = require('../homework.js')
 
-
-it('checkSeatStatus is a fonction', () => {
-    expect (typeof checkSeatStatus).toBe('function')
-    }
-);
-
-it('should throw a TypeError if first parameter is not a string', () => {
-    expect(() => checkSeatStatus(2)).toThrow(
-        new TypeError('First parameter it`s not a string')
-    )
+describe('checkSeatStatus function test', function(){
+    it('checkSeatStatus is a fonction', () => {
+        expect (typeof checkSeatStatus).toBe('function')
+        }
+    );
+    
+    it('should throw a TypeError if first parameter is not a string', () => {
+        expect(() => checkSeatStatus(2)).toThrow(
+            new TypeError('First parameter it`s not a string')
+        )
+    })
+    
+    it('should throw a TypeError if the second parameter is not a number', () => {
+        expect(() => checkSeatStatus('e', '2')).toThrow(
+            new TypeError('Second parameter it`s not a number')
+        )
+    })
+    it('should return true if the given seat bokking is not free', () => {
+        expect(checkSeatStatus('A', 1).toBeFalsy(true))
+    })
+    
+    it('should return false if the given seat bokking is free', () => {
+        expect(checkSeatStatus('E', 3).toBeTruty(false))
+    })
 })
 
-it('should throw a TypeError if the second parameter is not a number', () => {
-    expect(() => checkSeatStatus('e', '2')).toThrow(
-        new TypeError('Second parameter it`s not a number')
-    )
+describe('getRowNumber function test', function(){
+    it('sould return 0 if the letter is an "A"', () => {
+        expect(getRowNumber('A')).toBe(0)
+    });
+    
+    it('sould return 0 if the letter is an "A"', () => {
+        expect(getRowNumber('B')).toBe(1)
+    });
 })
 
-it('sould return 0 if the letter is an "A"', () => {
-    expect(getRowNumber('A')).toBe(0)
-});
-
-it('sould return 0 if the letter is an "A"', () => {
-    expect(getRowNumber('B')).toBe(1)
-});
-
-it('should return true if the given seat bokking is not free', () => {
-    expect(checkSeatStatus('A', 1).toBeFalsy(true))
+describe('book function test', function (){
+    it('should return "Seat in XX successfully booked" if the given seat is not booked', () => {
+        expect(book('E',3)).toBe('Seat in E3 successfully booked');
+      });
+      
+      it('should return "Seat in XX is already booked" if the given seat is already booked', () => {
+        expect(book('A',1)).toBe('Seat in A1 is already booked');
+      });
 })
 
-it('should return false if the given seat bokking is free', () => {
-    expect(checkSeatStatus('E', 3).toBeTruty(false))
-})
+
+
+
+
