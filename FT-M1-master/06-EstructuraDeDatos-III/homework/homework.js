81541f7dff54645a6bf8d6f0365411d91dbe39a6
n_narcise <joaquin.narcisse@gmail.com>
2022-11-16 18:43:31 -0500

    ok

--- FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
+++ FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
@@ -4,14 +4,114 @@
  Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
   - size: retorna la cantidad total de nodos del árbol
   - insert: agrega un nodo en el lugar correspondiente
-  - contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
-  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
-  - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
+  - contains: retorna true o false luego de evaluar si cierto 
+  valor existe dentro del árbol
+  - depthFirstForEach: recorre el árbol siguiendo el orden 
+  depth first (DFS) en cualquiera de sus variantes, 
+  según se indique por parámetro ("post-order", "pre-order", o "in-order").
+   Nota: si no se provee ningún parámetro, 
+   hará el recorrido "in-order" por defecto.
+  - breadthFirstForEach: recorre el árbol siguiendo el 
+  orden breadth first (BFS)
 
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.insert = function(value){
+  if(value > this.value){
+    if(this.right !== null){
+      this.right.insert(value);
+    }else{
+      this.right = new BinarySearchTree(value);
+    }
+  }
+  if(value < this.value){
+    if(this.left !== null){
+      this.left.insert(value)
+    }else{
+      this.left = new BinarySearchTree(value);
+    }
+  }
+};
+BinarySearchTree.prototype.contains = function(value){
+  if(value === this.value){
+    return true;
+  }
+  if(value > this.value){
+    if(this.right === null){
+      return false;
+    }else{
+      return this.right.contains(value);
+    }
+  }
+  if(value < this.value){
+    if(this.left ===null){
+      return false;
+    }else{
+      return this.left.contains(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
+  if(order === 'post-order'){
+    //izq - der - root
+    if(this.left !== null) this.left.depthFirstForEach(cb, order)
+    if(this.right !== null) this.right.depthFirstForEach(cb, order)
+    cb(this.value);
+  }else if(order === 'pre-order'){
+     //root - izq - der
+    cb(this.value);
+    if(this.left !== null) this.left.depthFirstForEach(cb, order);
+    if(this.right !== null) this.right.depthFirstForEach(cb, order);
+      }else{
+    // izq - root - der
+    if(this.left !== null) this.left.depthFirstForEach(cb, order)
+    cb(this.value);
+    if(this.right !== null) this.right.depthFirstForEach(cb, order);
+  }
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, array){
+  if (!array){
+    var array = [];
+  }
+  
+  if(this.left !== null) {
+    array.push(this.left);
+  }
+
+  if(this.right !== null) {
+    array.push(this.right);
+  }
+
+  cb(this.value);
+
+  if(array.length > 0){
+    array.shift().breadthFirstForEach(cb, array);
+  };
+};
+
+BinarySearchTree.prototype.size = function(){
+  if(this.left === null && this.right === null){
+    return 1;
+  }
+  if(this.left !== null && this.right === null){
+    return 1 + this.left.size();
+  }
+  if(this.left === null && this.right !== null){
+    return 1 + this.right.size();
+  }
+  if(this.left !== null && this.right !== null){
+    return 1 + this.left.size() + this.right.size();
+  }
+};
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
