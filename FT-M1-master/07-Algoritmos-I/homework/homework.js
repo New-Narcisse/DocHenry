854ffb4a3c3647c17503b1397571da3f4cd9429a
n_narcise <joaquin.narcisse@gmail.com>
2022-11-20 14:19:40 -0500

    finished hmwk algorithmI

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let array = [1];
+  let i = 2;
+  while(num !== 1){
+    if(num % i === 0){
+      array.push(i)
+      num /= i;
+    }else{
+      i++;
+    }
+  }
+return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let cambio = true;
+  while(cambio){
+    cambio = false;
+    for(let i = 0; i < array.length-1; i++){
+      if(array[i] > array[i + 1]){
+        let aux = array[i];
+        array[i] = array[i +1];
+        array[i + 1] = aux;
+        cambio = true
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +45,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 1; i < array.length; i++){
+    let j = i - 1;
+    let aux = array[i];
+    while(j >=0 && array[j] > aux){
+      array[j + 1] = array[j]
+      j--;
+    }
+    array[j + 1] = aux; 
+  }
+  return array;
 }
 
 
@@ -33,6 +64,20 @@ function selectionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for(let j = 0; j < array.length-1; j++){
+    let min = j;
+    for(let i = j + 1; i < array.length; i++){
+      if(array[i] < array[min]){
+        min = i;
+      }
+    }
+    if(j !== min){
+      let aux = array[j];
+      array[j] = array[min]
+      array[min] = aux;
+    }
+  }
+  return array;
 }
 
 
